40f8886597de7f09bcd08bca71fcaec7b8b8abf8
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-08-08 19:37:10 -0700

    prev and next buttons disabling if on first or last page

--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -4,13 +4,19 @@ import './Pokedex.css';
 
 export default class Pokedex extends Component {
     render() {
+        const pageArray = []
+
+        for(let i=1; i <= this.props.totalPages; i++ ){
+            pageArray.push(i)
+        }
+        console.log(pageArray)
         return (
             <div>
             <ul className='pageList'>
                 
-                <li><button key='prev' value="prev" onClick={this.props.pageButtonClick}>&lt;</button></li>
-                
-                <li><button key='next' value="next" onClick={this.props.pageButtonClick}>&gt;</button></li>
+                <li><button key='prev' value="prev" onClick={this.props.pageButtonClick} disabled={this.props.currentPage>1 ? false : true}>&lt;</button></li>
+                <li key='pageNumber'>Page {this.props.currentPage} of {this.props.totalPages}</li>
+                <li><button key='next' value="next" onClick={this.props.pageButtonClick}disabled={this.props.currentPage < this.props.totalPages ? false : true}>&gt;</button></li>
             </ul>
             <ul className='pokeList'>
                 {
