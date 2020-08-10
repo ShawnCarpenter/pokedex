912e6e551389b94eb63419f52024be9c241dfc72
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-08-10 00:42:01 -0700

    buttons working for pages now

--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -13,23 +13,24 @@ export default class Pokedex extends Component {
         const pageArray = []
 
         for(let i=1; i <= this.props.totalPages; i++ ){
-            
-            if(i >= (currentPage-3) && i <= (currentPage+3) )pageArray.push(i)
+            if(i >= (currentPage-3) && i <= (currentPage+3)) pageArray.push(i)
         }
         console.log(pageArray)
         
         return (
-            <div>
+            <div className='pokedexContainer'>
             <ul className='pageList'>
                 { totalPages ? <>
-                    <li><button key='prev' value="prev" onClick={pageButtonClick} disabled={currentPage > 1 ? false : true}>&lt;</button></li>
+                    <li><button key='prev' value="prev" className='pageListButton' onClick={pageButtonClick} disabled={currentPage > 1 ? false : true}>&lt;</button></li>
                     {
                         pageArray.map(page =>{
-                           return  <li key={page}><button>{page}</button></li>
+                           return  <li key={page}>
+                               <button value={page} onClick={pageButtonClick} className={page === currentPage? 'pageListButton currentPageButton': 'pageListButton'}>{page}</button>
+                            </li>
                         }) 
                     }
                     
-                    <li><button key='next' value="next" onClick={pageButtonClick}disabled={currentPage < totalPages ? false : true}>&gt;</button></li>
+                    <li><button key='next' value="next" className='pageListButton' onClick={pageButtonClick}disabled={currentPage < totalPages ? false : true}>&gt;</button></li>
                     </> : <h1>No results, try another search</h1>
                 }
                 
